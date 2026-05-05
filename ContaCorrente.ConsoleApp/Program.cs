ec5daa8991ad99b19ce9fa817d187fb236c39122
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-05-05 18:23:21 -0300

    Adicionado projeto inicial

---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("---------------------------------------------");
+    Console.WriteLine($"Conta Corrente");
+    Console.WriteLine("---------------------------------------------");
+    Console.WriteLine("1 - Saque");
+    Console.WriteLine("2 - Depósito");
+    Console.WriteLine("3 - Consulta de Saldo");
+    Console.WriteLine("S - Sair");
+    string? opcaoMenu = Console.ReadLine();
+
+    if (opcaoMenu?.ToUpper() == "S")
+        break;
+
+    if (opcaoMenu == "1")
+    {
+
+    }
+    else if (opcaoMenu == "2")
+    {
+
+    }
+    else if (opcaoMenu == "3")
+    {
+
+    }
+}
