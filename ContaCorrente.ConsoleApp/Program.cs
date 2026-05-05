cc510cd69ad9a98e494606efe503f9ce5b5cba1f
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-05-05 19:33:33 -0300

    Adicionada implementação sequencial

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,4 +1,14 @@
-﻿while (true)
+﻿/*
+Uma conta corrente é composta por diversos atributos essenciais para sua operação.
+Ela possui um número de identificação único, um saldo disponível e um limite de débito estabelecido.
+*/
+
+// Conta
+int id = 1;
+decimal saldo = 1200;
+decimal limiteDebito = 800;
+
+while (true)
 {
     Console.Clear();
     Console.WriteLine("---------------------------------------------");
@@ -11,20 +21,43 @@
     Console.WriteLine("---------------------------------------------");
     Console.Write("> ");
     string? opcaoMenu = Console.ReadLine();
+    Console.WriteLine("---------------------------------------------");
 
     if (opcaoMenu?.ToUpper() == "S")
         break;
 
     if (opcaoMenu == "1")
     {
+        Console.Write("Digite o valor do saque: R$ ");
+        decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
 
+        decimal limiteSaque = saldo + limiteDebito;
+
+        if (valorSaque > limiteSaque)
+        {
+            Console.WriteLine("Não é possível efetuar o saque. O limite foi ultrapassado.");
+            Console.ReadLine();
+            continue;
+        }
+
+        saldo -= valorSaque;
+
+        Console.WriteLine($"O saque de R$ {valorSaque} foi efetuado com sucesso.");
+        Console.ReadLine();
     }
     else if (opcaoMenu == "2")
     {
+        Console.Write("Digite o valor do depósito: R$ ");
+        decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
 
+        saldo += valorDeposito;
+
+        Console.WriteLine($"O depósito de R$ {valorDeposito} foi efetuado com sucesso.");
+        Console.ReadLine();
     }
     else if (opcaoMenu == "3")
     {
-
+        Console.WriteLine($"O saldo da conta é de: R$ {saldo}");
+        Console.ReadLine();
     }
 }
